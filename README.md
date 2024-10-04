6afec6df64efcd72830487ebff2ef64c8c48762d
nuuuri <sbfl125@gmail.com>
2024-10-04 23:58:28 +0900

    [feat] 프로젝트 셋팅

---
+++ README.md
@@ -0,0 +1,13 @@
+# 알고리즘 문제풀이 with TypeScript
+
+### 1. 패키지 설치
+
+```bash
+yarn // or yarn install
+```
+
+### 2. 코드 실행
+
+```bash
+yarn start
+```
---
+++ index.ts
@@ -0,0 +1,4 @@
+const greet = (name: string) => console.log(`hello, ${name}`);
+
+const names = ["bluemiv", "javascript", "typescript"];
+names.forEach(greet);
