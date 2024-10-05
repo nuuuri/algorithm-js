f9fd0352f8043a332ab90893b6d66f2753e4e7f2
nuuuri <sbfl125@gmail.com>
2024-10-05 17:50:31 +0900

    [feat] repeatable 속성 추가(중복순열, 중복조합 구현)

--- src/algorithm/combination.ts
+++ src/algorithm/combination.ts
@@ -1,5 +1,9 @@
 // 조합
-export default function combination<T>(list: T[], selectNumber: number) {
+export default function combination<T>(
+  list: T[],
+  selectNumber: number,
+  repeatable: boolean = false
+) {
   const results: T[][] = [];
 
   if (selectNumber === 1) return list.map((val) => [val]);
@@ -8,7 +12,7 @@ export default function combination<T>(list: T[], selectNumber: number) {
     // 현재 선택한 값 이후의 나머지 배열
     // 순서를 고려하지 않기 때문에, 현재 값 이전은 고려하지 않음
     // (이전 값은 이미 선택했거나, 선택하지 않기로 결정됨)
-    const rest = origin.slice(index + 1);
+    const rest = origin.slice(repeatable ? index : index + 1);
 
     // 나머지 배열에 대하여 조합을 구함
     const comb = combination(rest, selectNumber - 1);
--- src/algorithm/permutation.ts
+++ src/algorithm/permutation.ts
@@ -1,12 +1,18 @@
 // 순열
-export default function permutation<T>(list: T[], selectNumber: number) {
+export default function permutation<T>(
+  list: T[],
+  selectNumber: number,
+  repeatable: boolean = false
+) {
   const results: T[][] = [];
 
   if (selectNumber === 1) return list.map((val) => [val]);
 
   list.forEach((fixed, index, origin) => {
     // 현재 선택한 값을 제외한 나머지 배열
-    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const rest = repeatable
+      ? list
+      : [...origin.slice(0, index), ...origin.slice(index + 1)];
 
     // 나머지 배열에 대하여 순열을 구함
     const perm = permutation(rest, selectNumber - 1);
