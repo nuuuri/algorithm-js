7a5fc11c7f6cb34aa704d11a3217ab2d1c20b8e0
nuuuri <sbfl125@gmail.com>
2024-10-05 17:13:17 +0900

    [fix] 폴더 구조 변경

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// function solution() {
+//   /* */
+// }
+
+// const rl = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.on("line", function (line: string) {
+//   /* 입력 값 처리 부분 */
+
+//   rl.close(); // 입력 종료
+// }).on("close", function () {
+//   solution();
+//   process.exit();
+// });
+
+console.log("hello!");
