a7609779cdb526479361bb3fd68c7c152393454d
nuuuri <sbfl125@gmail.com>
2024-12-21 01:00:49 +0900

    [Level 3][연습문제] 야근 지수

---
+++ problems/Programmers/12927.js
@@ -0,0 +1,82 @@
+// [Level 3][연습문제] 야근 지수
+
+function solution(n, works) {
+  const maxHeap = new MaxHeap();
+  works.forEach((work) => maxHeap.push(work));
+
+  while (n > 0) {
+    let maxValue = maxHeap.pop();
+    if (maxValue) maxValue--;
+    maxHeap.push(maxValue);
+    n = n - 1;
+  }
+
+  return maxHeap.heap.reduce((acc, cur) => acc + cur ** 2, 0);
+}
+
+class MaxHeap {
+  heap = [];
+
+  constructor() {}
+
+  push(value) {
+    this.heap.push(value);
+    this._bubbleUp();
+  }
+
+  pop() {
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const max = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this._bubbleDown();
+    return max;
+  }
+
+  _swap(index1, index2) {
+    const temp = this.heap[index1];
+    this.heap[index1] = this.heap[index2];
+    this.heap[index2] = temp;
+  }
+
+  _bubbleUp() {
+    let index = this.heap.length - 1;
+
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+
+      if (this.heap[index] <= this.heap[parentIndex]) break;
+
+      this._swap(index, parentIndex);
+      index = parentIndex;
+    }
+  }
+
+  _bubbleDown() {
+    let index = 0;
+
+    while (true) {
+      let leftChildIndex = index * 2 + 1;
+      let rightChildIndex = leftChildIndex + 1;
+      let leftChild, rightChild;
+      let swapIndex = null;
+      const element = this.heap[index];
+
+      if (this.heap[leftChildIndex]) {
+        leftChild = this.heap[leftChildIndex];
+        if (element < leftChild) swapIndex = leftChildIndex;
+      }
+
+      if (this.heap[rightChildIndex]) {
+        rightChild = this.heap[rightChildIndex];
+        const value = swapIndex ? leftChild : element;
+        if (value < rightChild) swapIndex = rightChildIndex;
+      }
+
+      if (!swapIndex) break;
+
+      this._swap(index, swapIndex);
+      index = swapIndex;
+    }
+  }
+}
